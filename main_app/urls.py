55bcc657444196b01ca03241cb2aaed32d68552b
Chi Huynh <chi@skyhawk-277gt.local>
2019-09-10 16:56:20 -0700

    migrated models, templates for main_app for CBV

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,22 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+
 
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
-    path('coffees/', views.coffees_index, name = 'index')
+    path('coffees/', views.index, name = 'coffee_index'),
+    path('coffees/<int:pk>', views.CoffeeDetailView.as_view(), name = 'detail')
 ]
 
 
+# urlpatterns = [
+#   path('', views.home, name='home'),
+#   path('about/', views.about, name='about'),
+#   path('cats/', views.cats_index, name='index'),
+#   path('cats/<int:cat_id>/', views.cats_detail, name='detail'),
+#   path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
+#   path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
+#   path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
+#   path('cats/<int:cat_id>/add_feeding/', views.add_feeding, name='add_feeding')  
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from django.http import Http404
 from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from .models import Coffee
+
 
 def home(request):
-    return HttpResponse('hello')
+    return render(request, 'about.html')
 
 def about(request):
     return render(request, 'about.html')
 
-def coffees_index(request):
-    return HttpResponse('this is the index')
+def index(request):
+    coffees = Coffee.objects.all()
+    return render(request, 'coffees/index.html', { 'coffees': coffees })
     
 
+class CoffeeDetailView(DetailView):
+    model = Coffee
+    def coffee_detail_view(request, primary_key):
+        try:
+            coffee = Coffee.objects.get(pk = primary_key)
+        except Coffee.DoesNotExist:
+            raise Http404('Coffee Does Not Exist')
+
+        return render(request, 'coffees/detail.html', { 'coffee': coffee } )
 
 
